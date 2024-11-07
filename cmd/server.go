15d00772012dbd0f5c093a1556d8a3b2dd59ae78
sergio <sergiopopov@gmail.com>
2024-11-08 00:55:59 +0200

    Prepare route to get game status

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
-const version = "0.0.1"
+const version = "0.0.2"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello, Kitty. Version", version)
-	fmt.Println("Start server")
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger.Println("Hello, Kitty. Version", version)
+	logger.Println("Start server")
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+	flag.Parse()
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /v0/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("GET /v0/client/{id}", app.clientHandler)
+	mux.HandleFunc("GET /v0/game/status", app.gameStatusHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	app.logger.Println("healthcheckHandler ...")
+
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
+
+func (app *application) clientHandler(w http.ResponseWriter, r *http.Request) {
+	app.logger.Println("clientHandler ...")
+
+	clientId := r.PathValue("id")
+
+	fmt.Fprintf(w, "Client: %s\n", clientId)
+}
+
+func (app *application) gameStatusHandler(w http.ResponseWriter, r *http.Request) {
+	app.logger.Println("gameStatusHandler ...")
+
+	fmt.Fprintln(w, "Game status: ready")
+	fmt.Fprintf(w, "Client amount: %d\n", 0)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
