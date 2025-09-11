8bfe34731c0dc5f1e4cc96281f5d98655160350e
SPopov <sergiopopov@gmail.com>
2025-09-12 01:21:35 +0300

    Read app configs

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"expkit/internal/config"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+const (
+	CONFIG_DIR  = "configs"
+	CONFIG_FILE = "auth-server"
+)
+
 func main() {
 	fmt.Println("Auth service starting ...")
+
+	cfg, err := config.New(CONFIG_DIR, CONFIG_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("config: %+v\n", cfg)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
